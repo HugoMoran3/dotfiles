873616fe868623b5138e216cf096ea95708bf857
Hugo Moran <hugo.moran3@gmail.com>
2024-09-27 16:15:53 +0930

    Changed git ssh to git https

--- nerdfonts.sh
+++ nerdfonts.sh
@@ -27,17 +27,21 @@ if [[ ! -d "$fonts_dir" ]]; then
     mkdir -p "$fonts_dir"
 fi
 
-echo -e "\e[0;32mScript:\e[0m \e[0;34mClonning\e[0m \e[0;31mNerdFonts\e[0m \e[0;34mrepo (sparse)\e[0m"
-git clone --filter=blob:none --sparse git@github.com:ryanoasis/nerd-fonts
-cd nerd-fonts
+echo -e "\e[0;32mScript:\e[0m \e[0;34mCloning\e[0m \e[0;31mNerdFonts\e[0m \e[0;34mrepo (sparse)\e[0m"
+if ! git clone --filter=blob:none --sparse https://github.com/ryanoasis/nerd-fonts.git; then
+    echo -e "\e[0;31mError:\e[0m Failed to clone the Nerd Fonts repository"
+    exit 1
+fi
+
+cd nerd-fonts || exit 1
 
 for font in "${fonts[@]}"; do
-    echo -e "\e[0;32mScript:\e[0m \e[0;34mClonning font:\e[0m \e[0;31m${font}\e[0m"
+    echo -e "\e[0;32mScript:\e[0m \e[0;34mCloning font:\e[0m \e[0;31m${font}\e[0m"
     git sparse-checkout add "patched-fonts/${font}"
     echo -e "\e[0;32mScript:\e[0m \e[0;34mInstalling font:\e[0m \e[0;31m${font}\e[0m"
     ./install.sh "${font}"
 done
 
-echo -e "\e[0;32mScript:\e[0m \e[0;34mCleaning the mess...\e[0m"
-cd ../
-rm -rf nerd-fonts```
+echo -e "\e[0;32mScript:\e[0m \e[0;34mCleaning up...\e[0m"
+cd .. || exit 1
+rm -rf nerd-fonts
